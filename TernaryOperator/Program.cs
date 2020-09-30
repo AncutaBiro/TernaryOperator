243fe9cf3b52650367eb6b766f0713648a2246ea
AncutaBiro <ancuta.biro@yahoo.com>
2020-09-30 20:44:05 +0300

    Add project files.

---
+++ TernaryOperator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TernaryOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputData = Console.ReadLine();
+            int a = Convert.ToInt32(inputData);
+
+            inputData = Console.ReadLine();
+            int b = Convert.ToInt32(inputData);
+
+            int max = a < b ? b : a;
+
+            Console.WriteLine(max);
+            Console.Read();
+        }
+    }
+}
